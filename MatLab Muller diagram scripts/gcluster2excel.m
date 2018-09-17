0a83bce6a1aeab150906157081f6c45139e8b516
Unknown <cld100@pitt.edu>
2018-09-17 16:25:18 -0400

    Cleaned up uneeded files.

--- MatLab Muller diagram scripts/gcluster2excel.m
+++
@@ -1,9 +0,0 @@
-size = 25;
-gentrajectories = 0;
-rowLetter = 'A';
-
-for iter = 1:size
-    rowNum = num2str(iter);
-    Range = [rowLetter rowNum];
-    xlswrite(sprintf('%sgclusterOUT.xls', names(23:end-4)), allgenotypes(1,:,iter), 'Sheet1',Range);
-end
