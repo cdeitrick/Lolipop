d1550cfd543058b59c834cda3504170b9cf2ab2f
Unknown <cld100@pitt.edu>
2018-12-11 10:40:10 -0500

    Split calculate_genotypes into similarity and difference files

---
+++ muller/muller_genotypes/difference.py
@@ -0,0 +1,105 @@
+from typing import List, Tuple
+import itertools
+import pandas
+
+try:
+	from muller.muller_genotypes.similarity import PairwiseArrayType
+except ModuleNotFoundError:
+	from .similarity import PairwiseArrayType
+
+def _divide_genotype(genotype: List[str], unlinked_trajectories: pandas.DataFrame,
+		pair_array: PairwiseArrayType, link_cut: float) -> Tuple[List[str], List[str]]:
+	"""
+		Splits a genotype into smaller muller_genotypes if some members are not related to some other members. This may happen
+		when a member is included into a genotype due to its paired member but ends up not related to some of
+		the other members that where found later.
+	Parameters
+	----------
+	genotype: Genotype
+		The genotype to split.
+	unlinked_trajectories: pandas.DataFrame
+		A dataframe of trajectory pairs and their corresponding p-value.
+		Columns:
+			- left
+			- right
+			- pvalue
+	pair_array
+		The list of p-values for each pair.
+	link_cut: float
+		The cuttoff value to choose whether a member is related to the other members.
+
+	Returns
+	-------
+		a tuple of two muller_genotypes.
+	"""
+	# Find the index of the minimum p-value after subtracting the link cutoff.
+	minimum_pvalue_index = (unlinked_trajectories['pvalue'] - link_cut).abs().idxmin()
+	# Get the row with the identified minimu mp-value.
+	minimum_pvalue_genotype = unlinked_trajectories.loc[minimum_pvalue_index]
+
+	# Form two new muller_genotypes based on the two trajectories corresponding to the minimum p-value
+	new_genotype_1_base = minimum_pvalue_genotype['left']
+	new_genotype_2_base = minimum_pvalue_genotype['right']
+
+	# Make sure genotype 1 includes the lower id-value. Not important, but maintains parity with matlab script.
+	new_genotype_1_base, new_genotype_2_base = sorted([new_genotype_1_base, new_genotype_2_base])
+	new_genotype_1 = [new_genotype_1_base]
+	new_genotype_2 = [new_genotype_2_base]
+
+	# Sort each member in the current genotype into one of the new muller_genotypes.
+	for genotype_member in genotype:
+		genotype_member = genotype_member
+		# Check if the current genotype member is already contained in one of the muller_genotypes.
+		# Should only be one of the two trajectories used to form a new genotype.
+		if genotype_member in new_genotype_1 or genotype_member in new_genotype_2:
+			pass
+		else:
+			# Use the highest p-value to determine which genotype to add the member to.
+			# P-values should correspond to the current member and the base member of the new muller_genotypes.
+			p_value_1 = pair_array[new_genotype_1_base, genotype_member]
+			p_value_2 = pair_array[new_genotype_2_base, genotype_member]
+
+			if p_value_1 >= p_value_2:
+				new_genotype_1.append(genotype_member)
+			else:
+				new_genotype_2.append(genotype_member)
+	return new_genotype_1, new_genotype_2
+
+
+def unlink_unrelated_trajectories(all_genotypes: List[List[str]], pair_array: PairwiseArrayType, link_cutoff: float) -> List[List[str]]:
+	"""
+		Splits each genotype if any of its members are not related enough to the other members. Genotypes will continue
+		to be split until the p-values for all pairwise members are beneath the cutoff.
+	Parameters
+	----------
+	all_genotypes: List[Genotype]
+		A list of all muller_genotypes.
+	pair_array: PairWiseArrayType
+		A mapping of all pairwise p-values.
+	link_cutoff: float
+		The cuttoff value to determine if a given pair of trajectories is unrelated.
+
+	Returns
+	-------
+		A list of the new muller_genotypes.
+
+	"""
+	for genotype in all_genotypes:
+		if len(genotype) > 1:
+			# Iterate over all possible pairs of genotype members.
+			combination_pairs = [(left, right, pair_array[left, right]) for left, right in itertools.combinations(genotype, 2)]
+			# Combine all pairs and p-values into a dataframe for convienience.
+			genotype_combinations = pandas.DataFrame(combination_pairs, columns = ['left', 'right', 'pvalue'])
+
+			# Get a dataframe of all trajectories in this genotype which are significantly different than the
+			# current pair of trajectories.
+			unlinked_trajectories = genotype_combinations[genotype_combinations['pvalue'] < link_cutoff]
+
+			if len(unlinked_trajectories) != 0:
+				# Split the current genotype into two smaller but more internally-related all_genotypes.
+				new_genotype_1, new_genotype_2 = _divide_genotype(genotype[:], genotype_combinations, pair_array, link_cutoff)
+
+				all_genotypes.append(new_genotype_1)
+				all_genotypes.append(new_genotype_2)
+				all_genotypes.remove(genotype)
+	return sorted(all_genotypes, key = len)
---
+++ muller/muller_genotypes/similarity.py
@@ -0,0 +1,113 @@
+import itertools
+import math
+from typing import List, Tuple, Dict
+
+import pandas
+PairwiseArrayType = Dict[Tuple[str, str], float]
+
+def calculate_p_value(left: pandas.Series, right: pandas.Series, detected_cutoff: float, fixed_cutoff: float) -> float:
+	"""
+		Calculates the relative similarity between all trajectory pairs.
+
+		these are consistent with n independent draws from a normal distribution,
+        assuming an average variance of  n_{binom} p (1-p), where p is the
+        average of the two frequencies, and n_{binom} is picked arbitrarily as a
+        value that gives reasonable uncertainties given our data
+
+        n random draws will have
+        \sigma_{tot}^2 = \sum_i \sigma_i^2 =  n_{binom} \sum_i p_i(1 - p_i), from
+        a property of normal distributions
+
+        for \bar{X} = \sum_{i}^{n_X} X/n_X
+
+        \sigma_{\bar{X}} = \sigma_{tot}/n_X = \sqrt( \sum_i p_i(1-p_i))/\sqrt{n_X}
+
+        Finally, given \sigma and \bar{X} we can construct a p-value for
+        the measurement by numerically computing the following integral:
+
+        1 - \int_{- \bar{X}/ \sigma_{\bar{X}}}^{\bar{X}/
+        \sigma_{\bar{X}}} \frac{1}{\sqrt{2 \pi}} e^{-x^2/2} dx
+	Parameters
+	----------
+	left, right: pandas.Series
+		- index: int
+			Timepoints
+		- values: float
+			Frequencies
+	detected_cutoff: float
+	fixed_cutoff: float
+
+
+	Returns
+	-------
+	float
+	"""
+	# Merge into a dataframe for convienience
+	df = pandas.concat([left, right], axis = 1)
+
+	# Remove timepoints where at least one trajectory was not fixed or undetected.
+
+	not_detected_fixed_df = df[(df < fixed_cutoff).any(axis = 1) & (df > detected_cutoff).any(axis = 1)]
+
+	if not_detected_fixed_df.empty:
+		left_fixed: pandas.Series = left[left > fixed_cutoff]
+		right_fixed: pandas.Series = right[right > fixed_cutoff]
+
+		if left_fixed.empty and right_fixed.empty:
+			# Both are undetected
+			p_value = 1
+		else:
+			overlap = set(left_fixed.index) and set(right_fixed.index)
+			p_value = int(len(left_fixed) > 2 and len(right_fixed) > 2 and len(overlap) > 2)
+
+	else:
+		# Find the mean frequency of each timepoint
+		# index is timepoints,  values are frequencies
+		mean: pandas.Series = not_detected_fixed_df.mean(axis = 1)
+
+		# Calculate sigma_freq
+		# E(sigma) = (1/n) sum(sigma) = (1/n) sum(np(1-p)) == sum(p(1-p)
+		# E(sigma_p) = (1/n) E(sigma) == 1/n(sum(p(1-p))
+		# E(d_bar) = 1/n(sum(di)) == 1/n (n*sum(di))
+		# pandas.Series.radd is slow for some reason.
+		sigma_freq: pandas.Series = mean.mul(1 - mean)
+		# Difference of frequencies at each timepoint
+		difference: pandas.Series = not_detected_fixed_df.iloc[:, 0] - not_detected_fixed_df.iloc[:, 1]
+		sigma_pair: float = sigma_freq.sum() / len(mean)
+		# Sum of differences
+		difference_mean: float = abs(difference).sum()
+
+		X = difference_mean / (math.sqrt(2 * sigma_pair))
+
+		p_value: float = 1 - math.erf(X)
+
+	return p_value
+
+
+def calculate_pairwise_trajectory_similarity(trajectories: pandas.DataFrame, detection_cutoff: float, fixed_cutoff: float) -> PairwiseArrayType:
+	"""
+	Parameters
+	----------
+	trajectories: pandas.DataFrame
+		A table of mutational trajectories. Should be a normal trajectory table.
+	detection_cutoff: float
+	fixed_cutoff: float
+
+	Returns
+	-------
+	dict of PairArrayValue
+	Each key in the dictionary corresponds to a pair of trajectory ids which map to the p-value for that pair.
+	The order of ids does not matter.
+	"""
+	pair_combinations: List[Tuple[str, str]] = itertools.combinations(trajectories.index, 2)
+	pair_array = dict()
+	for left, right in pair_combinations:
+		left_trajectory = trajectories.loc[left]
+		right_trajectory = trajectories.loc[right]
+
+		p_value = calculate_p_value(left_trajectory, right_trajectory, detection_cutoff, fixed_cutoff)
+
+		pair_array[left, right] = p_value
+		pair_array[right, left] = p_value
+
+	return pair_array
