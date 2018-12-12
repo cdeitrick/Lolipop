e71ad27ad85a61d5e30ce303167cdab09b81128a
Unknown <cld100@pitt.edu>
2018-12-12 11:18:53 -0500

    Now checks if seaborn is installed.

--- muller/graphics/heatmap.py
+++ muller/graphics/heatmap.py
@@ -1,12 +1,18 @@
-import seaborn
+
 import matplotlib.pyplot as plt
 import pandas
 from pathlib import Path
+try:
+	import seaborn
+except ModuleNotFoundError:
+	# Seaborn is not installed. Skip plotting
+	seaborn = None
 def plot_heatmap(data:pandas.DataFrame, filename:Path):
 	figsize = (20, 20)
 	fig, ax = plt.subplots(figsize = figsize)
 	ax.set_ylabel("Trajectory Label")
 	ax.set_xlabel("Trajectory Label")
 	ax.set_title("p-values of all mutational trajectories")
-	seaborn.heatmap(data, ax = ax)
-	fig.savefig(str(filename), format = 'png')
+	if seaborn is not None:
+		seaborn.heatmap(data, ax = ax)
+		fig.savefig(str(filename), format = 'png')
