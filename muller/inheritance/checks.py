e6a781fdc2e2144fd76ebd49198efb5f58ae1788
Unknown <cld100@pitt.edu>
2019-01-31 15:29:56 -0500

    Slight change to the difference check, and added docstrings.

--- muller/inheritance/checks.py
+++ muller/inheritance/checks.py
@@ -1,8 +1,22 @@
+from typing import Any, Optional, Tuple
+
 import pandas
 
-def check_additive_background(left: pandas.Series, right: pandas.Series, double_cutoff: float,
-		single_cutoff: float) -> bool:
-	# Want to check if a genotype's sum of frequencies is consistently greater than 1.0
+
+def check_additive_background(left: pandas.Series, right: pandas.Series, double_cutoff: float, single_cutoff: float) -> bool:
+	"""
+		Want to check if the sum of two genotypes at one or more timepoints is consistently greater than 1.0
+	Parameters
+	----------
+	left, right: pandas.Series
+	double_cutoff: float
+		Returns True if two or more timepoints sum to a value that exceeds this frequency.
+	single_cutoff
+		Returns True if one or more timepoints sum to a value that exceeds this frequency.
+	Returns
+	-------
+	bool
+	"""
 	trajectorysum = right + left
 	double_check = (trajectorysum > double_cutoff).sum() > 0  # Implicit conversion from bool to int.
 	single_check = (trajectorysum > single_cutoff).sum() > 0
@@ -11,8 +25,21 @@ def check_additive_background(left: pandas.Series, right: pandas.Series, double_
 
 def check_subtractive_background(left: pandas.Series, right: pandas.Series, double_cutoff: float,
 		single_cutoff: float) -> bool:
+	"""
+		Check if the one genotype is significantly larger than the other.
+	Parameters
+	----------
+	left: pandas.Series
+	right: pandas.Series
+	double_cutoff: float
+	single_cutoff: float
+
+	Returns
+	-------
+
+	"""
 	# Check if the current genotype is over 15% larger than the other.
-	diff_trajectory = right - left
+	diff_trajectory = -abs(right - left)
 	double_diff_trajectory = (diff_trajectory < double_cutoff).sum() > 1
 	single_diff_trajectory = (diff_trajectory < single_cutoff).sum() > 0  # implicit conversion from bool to int
 	return double_diff_trajectory or single_diff_trajectory
@@ -43,3 +70,31 @@ def check_derivative_background(left: pandas.Series, right: pandas.Series, detec
 	delta = left_window.dot(right_window)
 
 	return delta
+
+
+def apply_genotype_checks(type_trajectory: pandas.Series, test_trajectory: pandas.Series, options: Any) -> Tuple[bool, bool, Optional[float]]:
+	""" Applies the three checks to `type_trajectory` and `test_trajectory`."""
+	additive_check = check_additive_background(
+		left = type_trajectory,
+		right = test_trajectory,
+		double_cutoff = options.additive_background_double_cutoff,
+		single_cutoff = options.additive_background_single_cutoff
+	)
+
+	subtractive_check = check_subtractive_background(
+		left = type_trajectory,
+		right = test_trajectory,
+		double_cutoff = options.subtractive_background_double_cutoff,
+		single_cutoff = options.subtractive_background_single_cutoff
+	)
+
+	if subtractive_check:
+		delta = None
+	else:
+		delta = check_derivative_background(
+			left = type_trajectory,
+			right = test_trajectory,
+			detection_cutoff = options.derivative_detection_cutoff
+		)
+
+	return additive_check, subtractive_check, delta
