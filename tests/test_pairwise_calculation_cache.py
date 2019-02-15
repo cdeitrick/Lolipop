52fc1b9c971fdbc1c850447d90a7fb0865b1af00
Unknown <cld100@pitt.edu>
2019-02-15 17:00:21 -0500

    Renamed Pairwise Calculation Cache

--- tests/test_pairwise_calculation_cache.py
+++ tests/test_pairwise_calculation_cache.py
@@ -1,17 +1,17 @@
 import pytest
-from clustering.metrics import PairwiseCalculation
+from clustering.metrics import PairwiseCalculationCache
 import itertools
 
 @pytest.fixture
 def empty_cache():
-	return PairwiseCalculation()
+	return PairwiseCalculationCache()
 
 @pytest.fixture
 def small_cache():
 	elements = ['element1', 'element2', 'element3']
 	combinations = itertools.combinations(elements, 2)
 	pair_array = {k:'|'.join(k) for k in combinations}
-	return PairwiseCalculation(pair_array)
+	return PairwiseCalculationCache(pair_array)
 
 def test_cache_empty(empty_cache, small_cache):
 	assert bool(empty_cache) == False
