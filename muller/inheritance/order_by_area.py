a9a300b267d1033f5f5f6554a0331eaafbe0ca7c
Unknown <cld100@pitt.edu>
2019-02-26 12:47:44 -0500

    Added jaccard subset check
    Removed unused functions, removed the dlimit parameter, and implemented the jaccard subset check.

--- muller/inheritance/order_by_area.py
+++ muller/inheritance/order_by_area.py
@@ -1,3 +1,4 @@
+import math
 from typing import Set
 
 import pandas
@@ -9,61 +10,64 @@ def area_of_series(series: pandas.Series) -> float:
 	return total
 
 
-def calculate_noncommon_area(left: pandas.Series, right: pandas.Series) -> float:
-	""" Calculates the area not contained by bboth `left` and `right`"""
-	difference = abs(left - right)
-	noncommon_area = area_of_series(difference)
-
-	return noncommon_area
-
-
-def calculate_non_overlapping_area(left: pandas.Series, right: pandas.Series, dlimit) -> float:
-	# Area of the timepoionts that do no overlap
-	overlap_index = get_overlap(left, right, dlimit)
-	nonoverlap_left = left.loc[~left.index.isin(overlap_index)]
-	nonoverlap_right = right.loc[~right.index.isin(overlap_index)]
-	area_left = area_of_series(nonoverlap_left)
-	area_right = area_of_series(nonoverlap_right)
-	return area_left + area_right
-
-
-def calculate_common_area(left: pandas.Series, right: pandas.Series, dlimit: float) -> float:
-	# left = left.reset_index(drop = True)
-	# right = right.reset_index(drop = True)
-	overlap_index = get_overlap(left, right, dlimit)
+def calculate_common_area(left: pandas.Series, right: pandas.Series) -> float:
+	""" Calculates |X and Y|"""
+	overlap_index = get_overlap(left, right)
 	if not overlap_index:
 		overlap = 0
 	else:
 		overlap = area_of_series(left.where(left < right, right))
 	return overlap
 
-def calculate_area_difference(left: pandas.Series, right:pandas.Series):
+
+def calculate_area_difference(left: pandas.Series, right: pandas.Series):
 	left_area = area_of_series(left)
 	right_area = area_of_series(right)
 	return left_area - right_area
+
+
 # noinspection PyTypeChecker
-def get_overlap(left: pandas.Series, right: pandas.Series, dlimit: float) -> Set[int]:
-	detected_left = left[left > dlimit]
-	detected_right = right[right > dlimit]
+def get_overlap(left: pandas.Series, right: pandas.Series) -> Set[int]:
+	detected_left = left
+	detected_right = right
 
 	overlap_index = set(detected_left.index) & set(detected_right.index)
 	return overlap_index
 
 
-# noinspection PyTypeChecker
-def compare_to_table(left: pandas.Series, table: pandas.DataFrame, dlimit: float) -> pandas.DataFrame:
-	area = area_of_series(left)
-	axis = 1
-	common_area = table.apply(calculate_common_area, axis = axis, args = (left, dlimit)) / area
-	noncommon_area = table.apply(calculate_noncommon_area, axis = axis, args = (left, dlimit)) / area
-	nonoverlapping_area = table.apply(calculate_non_overlapping_area, axis = axis, args = (left, dlimit)) / area
+def X_or_Y(left: pandas.Series, right: pandas.Series) -> float:
+	area_left = area_of_series(left)
+	area_right = area_of_series(right)
+	common_area = calculate_common_area(left, right)
 
-	df = pandas.concat([common_area, noncommon_area, nonoverlapping_area], axis = 1)
-	df.columns = ['commonArea', 'noncommonArea', 'nonoverlappingArea']
-	df = df.sort_values(by = ['commonArea', 'nonoverlappingArea', 'noncommonArea'], ascending = [False, True, True])
-	return df
+	return area_left + area_right - common_area
 
 
+def jaccard_distance(left: pandas.Series, right: pandas.Series) -> float:
+	x_or_y = X_or_Y(left, right)
+	return (x_or_y - X_and_Y(left, right)) / x_or_y
+
+
+def jaccard_subset(left: pandas.Series, right: pandas.Series) -> float:
+	left_area = area_of_series(left)
+	right_area = area_of_series(right)
+	return (left_area - right_area) / left_area
+
+
+def is_subset(left: pandas.Series, right: pandas.Series) -> bool:
+	""" Tests if `right` is a subset of `left`"""
+	actual_jaccard_distance = jaccard_distance(left, right)
+	subset_jaccard_distance = jaccard_subset(left, right)
+
+	# TODO: incorporate variance into this
+	return math.isclose(actual_jaccard_distance, subset_jaccard_distance, abs_tol = 0.1)
+
+
+X_and_Y = calculate_common_area
 
 if __name__ == "__main__":
-	pass
+	left = pandas.Series([0, .1, .2, .3, .4])
+	right = pandas.Series([0, 0, .1, .2, .3])
+
+	print(is_subset(left, right))
+	print(is_subset(right, left))
