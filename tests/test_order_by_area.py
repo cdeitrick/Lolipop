0c4fed9448f0bce8743fc80ba1c3dcffc71dce62
Unknown <cld100@pitt.edu>
2019-02-26 14:22:39 -0500

    Updated tests

--- tests/test_order_by_area.py
+++ tests/test_order_by_area.py
@@ -1,21 +1,27 @@
-import pytest
-import pandas
-from inheritance import order_by_area
 from typing import Tuple
-@pytest.fixture
-def full_overlap()->Tuple[pandas.Series, pandas.Series]:
-	left = pandas.Series([0, 0, 0, 0, 1, 1])
-	right=  pandas.Series([0, 0, 0, 0, .2, .3])
-	return left, right
+
+import pandas
+import pytest
+
+from inheritance import order_by_area
+
 
 @pytest.fixture
-def no_overlap()->Tuple[pandas.Series, pandas.Series]:
-	left = pandas.Series([0,.3, .4, 0, 0, 0])
+def full_overlap() -> Tuple[pandas.Series, pandas.Series]:
+	left = pandas.Series([0, 0, 0, 0, 1, 1])
 	right = pandas.Series([0, 0, 0, 0, .2, .3])
 	return left, right
 
+
 @pytest.fixture
-def partial_overlap()->Tuple[pandas.Series, pandas.Series]:
+def no_overlap() -> Tuple[pandas.Series, pandas.Series]:
+	left = pandas.Series([0, .3, .4, 0, 0, 0])
+	right = pandas.Series([0, 0, 0, 0, .2, .3])
+	return left, right
+
+
+@pytest.fixture
+def partial_overlap() -> Tuple[pandas.Series, pandas.Series]:
 	left = pandas.Series([0, 0, .1, .2, 1, 1])
 	right = pandas.Series([0, 0, 0, 0, .2, .3])
 	return left, right
@@ -24,33 +30,30 @@ def partial_overlap()->Tuple[pandas.Series, pandas.Series]:
 def test_calculate_overlapping_area(full_overlap, no_overlap, partial_overlap):
 	left, right = full_overlap
 	right_area = order_by_area.area_of_series(right)
-	overlap_area = order_by_area.calculate_common_area(left, right, 0.03)
+	overlap_area = order_by_area.calculate_common_area(left, right)
 	assert overlap_area == right_area
 
 	left, right = no_overlap
-	overlap_area = order_by_area.calculate_common_area(left, right, 0.03)
+	overlap_area = order_by_area.calculate_common_area(left, right)
 	assert overlap_area == 0
 
 	left, right = partial_overlap
-	overlap_area = order_by_area.calculate_common_area(left, right, 0.03)
+	overlap_area = order_by_area.calculate_common_area(left, right)
 	assert overlap_area == right_area
 
-	left = pandas.Series([0.01,0.279,0.341,0.568,0.708,0.913,0.756,0.455,0.399,0.13,0.041])
-	right = pandas.Series([0,0,0,0,0,0.247,0.388,0.215,0.403,0.141,0.028])
-	overlap_area = order_by_area.calculate_common_area(left, right, 0)
+	left = pandas.Series([0.01, 0.279, 0.341, 0.568, 0.708, 0.913, 0.756, 0.455, 0.399, 0.13, 0.041])
+	right = pandas.Series([0, 0, 0, 0, 0, 0.247, 0.388, 0.215, 0.399, 0.13, 0.028])
+	overlap_area = order_by_area.calculate_common_area(left, right)
 	assert overlap_area == order_by_area.area_of_series(right)
-def test_calculate_noncommon_area(full_overlap, partial_overlap, no_overlap):
+
+def test_is_subset(full_overlap, no_overlap, partial_overlap):
 	left, right = full_overlap
-	expected_area = abs(order_by_area.area_of_series(right) - order_by_area.area_of_series(left))
-	nonoverlap_area = order_by_area.calculate_noncommon_area(left, right, 0.03)
-	assert nonoverlap_area == expected_area
+	assert order_by_area.is_subset(left, right)
+	assert False == order_by_area.is_subset(right,left)
 
 	left, right = partial_overlap
-	expected_area = order_by_area.area_of_series(pandas.Series([.1, .2, .8, .7]))
-	nonoverlap_area = order_by_area.calculate_noncommon_area(left, right, 0.03)
-	assert nonoverlap_area == expected_area
-
+	assert True == order_by_area.is_subset(left, right)
+	assert False == order_by_area.is_subset(right, left)
 	left, right = no_overlap
-	expected_area = order_by_area.area_of_series(left) + order_by_area.area_of_series(right)
-	noncommon_area = order_by_area.calculate_noncommon_area(left, right, .03)
-	assert expected_area == noncommon_area
+	assert False == order_by_area.is_subset(left, right)
+	assert False == order_by_area.is_subset(right, left)
