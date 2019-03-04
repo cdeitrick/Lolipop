d0aa33355336fa3772da8e823660c61296960dfe
Unknown <cld100@pitt.edu>
2019-03-04 10:37:53 -0500

    Refactored difference calculation
    removed a required parameter and switched to permutations

--- muller/clustering/methods/difference.py
+++ muller/clustering/methods/difference.py
@@ -4,8 +4,7 @@ from typing import Dict, List, Tuple
 import pandas
 
 
-def _divide_genotype(genotype: List[str], unlinked_trajectories: pandas.DataFrame,
-		pair_array: Dict[Tuple[str, str], float], link_cut: float) -> Tuple[List[str], List[str]]:
+def _divide_genotype(genotype: List[str], unlinked_trajectories: pandas.Series, link_cut: float) -> Tuple[List[str], List[str]]:
 	"""
 		Splits a genotype into smaller muller_genotypes if some members are not related to some other members. This may happen
 		when a member is included into a genotype due to its paired member but ends up not related to some of
@@ -14,7 +13,7 @@ def _divide_genotype(genotype: List[str], unlinked_trajectories: pandas.DataFram
 	----------
 	genotype: Genotype
 		The genotype to split.
-	unlinked_trajectories: pandas.DataFrame
+	unlinked_trajectories: pandas.Series
 		A dataframe of trajectory pairs and their corresponding p-value.
 		Columns:
 			- left
@@ -30,13 +29,9 @@ def _divide_genotype(genotype: List[str], unlinked_trajectories: pandas.DataFram
 		a tuple of two muller_genotypes.
 	"""
 	# Find the index of the minimum p-value after subtracting the link cutoff.
-	minimum_pvalue_index = (unlinked_trajectories['pvalue'] - link_cut).abs().idxmin()
-	# Get the row with the identified minimu mp-value.
-	minimum_pvalue_genotype = unlinked_trajectories.loc[minimum_pvalue_index]
-
 	# Form two new muller_genotypes based on the two trajectories corresponding to the minimum p-value
-	new_genotype_1_base = minimum_pvalue_genotype['left']
-	new_genotype_2_base = minimum_pvalue_genotype['right']
+	new_genotype_1_base, new_genotype_2_base = (unlinked_trajectories - link_cut).abs().idxmin()
+	# Get the row with the identified minimum mp-value.
 
 	# Make sure genotype 1 includes the lower id-value. Not important, but maintains parity with matlab script.
 	new_genotype_1_base, new_genotype_2_base = sorted([new_genotype_1_base, new_genotype_2_base])
@@ -45,7 +40,6 @@ def _divide_genotype(genotype: List[str], unlinked_trajectories: pandas.DataFram
 
 	# Sort each member in the current genotype into one of the new muller_genotypes.
 	for genotype_member in genotype:
-		genotype_member = genotype_member
 		# Check if the current genotype member is already contained in one of the muller_genotypes.
 		# Should only be one of the two trajectories used to form a new genotype.
 		if genotype_member in new_genotype_1 or genotype_member in new_genotype_2:
@@ -53,8 +47,8 @@ def _divide_genotype(genotype: List[str], unlinked_trajectories: pandas.DataFram
 		else:
 			# Use the highest p-value to determine which genotype to add the member to.
 			# P-values should correspond to the current member and the base member of the new muller_genotypes.
-			p_value_1 = pair_array[new_genotype_1_base, genotype_member]
-			p_value_2 = pair_array[new_genotype_2_base, genotype_member]
+			p_value_1 = unlinked_trajectories.loc[new_genotype_1_base, genotype_member]
+			p_value_2 = unlinked_trajectories.loc[new_genotype_2_base, genotype_member]
 
 			if p_value_1 >= p_value_2:
 				new_genotype_1.append(genotype_member)
@@ -84,16 +78,17 @@ def unlink_unrelated_trajectories(all_genotypes: List[List[str]], pair_array: Di
 	for genotype in all_genotypes:
 		if len(genotype) > 1:
 			# Iterate over all possible pairs of genotype members.
-			combination_pairs = [(left, right, pair_array[left, right]) for left, right in itertools.combinations(genotype, 2)]
+			combination_pairs = [(left, right, pair_array[left, right]) for left, right in itertools.permutations(genotype, 2)]
 			# Combine all pairs and p-values into a dataframe for convienience.
 			genotype_combinations = pandas.DataFrame(combination_pairs, columns = ['left', 'right', 'pvalue'])
+			genotype_combinations:pandas.Series = genotype_combinations.set_index(['left', 'right'])['pvalue']
 			# Get a dataframe of all trajectories in this genotype which are significantly different than the
 			# current pair of trajectories.
-			unlinked_trajectories = genotype_combinations[genotype_combinations['pvalue'] < link_cutoff]
+			unlinked_trajectories = genotype_combinations[genotype_combinations < link_cutoff].dropna()
 
 			if len(unlinked_trajectories) != 0:
 				# Split the current genotype into two smaller but more internally-related all_genotypes.
-				new_genotype_1, new_genotype_2 = _divide_genotype(genotype[:], genotype_combinations, pair_array, link_cutoff)
+				new_genotype_1, new_genotype_2 = _divide_genotype(genotype[:], genotype_combinations, link_cutoff)
 
 				all_genotypes.append(new_genotype_1)
 				all_genotypes.append(new_genotype_2)
