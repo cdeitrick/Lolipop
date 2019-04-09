fc2f11bee1a43bfbc8e1a516f6dbc9674b9f2a56
cld100 <cld100@pitt.edu>
2019-04-09 13:55:15 -0400

    Removed unused funcitons

--- muller/graphics/dendrogram.py
+++ muller/graphics/dendrogram.py
@@ -10,28 +10,6 @@ except ModuleNotFoundError:
 	from ..clustering.metrics import PairwiseCalculationCache
 
 
-def get_dendrogram_colors(Z, trajectory_colors: Dict[str, str]) -> Dict[int, str]:
-	# see question for code prior to "color mapping"
-
-	# Color mapping
-	trajectory_colors["0"] = "#FF0000"
-	dflt_col = "#808080"  # Unclustered gray
-	# notes:
-	# * rows in Z correspond to "inverted U" links that connect clusters
-	# * rows are ordered by increasing distance
-	# * if the colors of the connected clusters match, use that color for link
-	index_to_trajectory = lambda s: str(s)
-
-	link_cols: Dict[int, str] = {}
-	for cluster_index, (left_label, right_label) in enumerate(Z[:, :2].astype(int)):
-		get_color = lambda s: link_cols[s] if s > len(Z) else trajectory_colors[index_to_trajectory(s)]
-		color_1 = get_color(left_label)
-		color_2 = get_color(right_label)
-		link_cols[cluster_index + 1 + len(Z)] = color_1 if color_1 == color_2 else dflt_col
-
-	return link_cols
-
-
 def plot_dendrogram(Z: Any, pair_array: PairwiseCalculationCache, filename: Path):
 	fig, ax = plt.subplots(figsize = (15, 15))
 	#plt.figure(figsize = (15, 15))
