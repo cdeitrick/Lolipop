f3700a0a3e7240162e809ba67f2a7a83f6ef4916
cld100 <cld100@pitt.edu>
2019-06-11 15:36:38 -0400

    Bump version and enable debug

--- muller/commandline_parser.py
+++ muller/commandline_parser.py
@@ -11,8 +11,8 @@ except ModuleNotFoundError:
 
 from dataclasses import dataclass, fields
 
-__VERSION__ = "0.5.1"
-DEBUG = False
+__VERSION__ = "0.5.2"
+DEBUG = True
 
 # For convienience. Helps with autocomplete.
 @dataclass
