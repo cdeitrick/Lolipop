1e896db438635de769835afe3756480cb67e7ecb
cld100 <cld100@pitt.edu>
2019-08-15 14:11:33 -0400

    fixed minor bug

--- muller/commandline_parser.py
+++ muller/commandline_parser.py
@@ -173,8 +173,8 @@ def create_parser() -> argparse.ArgumentParser:
 
 	parser.add_argument(
 		"--filename-pairwise",
-		help = "Path to a table with pairwise distance calculations from a previous run using identical input parameters. Should be located " \ 
-			"in `tables/.distances.tsv in the output folder generated from the previous run. This table will be used rather than re-calculating " \ 
+		help = "Path to a table with pairwise distance calculations from a previous run using identical input parameters. Should be located " \
+			"in `tables/.distances.tsv in the output folder generated from the previous run. This table will be used rather than re-calculating " \
 			"all the pairwise distances again which may take a long time for very large datasets.",
 		action = "store",
 		dest = "filename_pairwise",
